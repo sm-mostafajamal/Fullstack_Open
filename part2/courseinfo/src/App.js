378b506c7ec2d3cd5716b7c54f9cfebbf400eaf7
sm-mostafajamal <sm.mostafajamal@gmail.com>
2022-08-12 04:25:45 +0600

    2.5: separate module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,28 +1,5 @@
 import {useState} from 'react';
-
-const Header = ({course}) => <h3>{course}</h3>
-
-const Content = ({parts}) => <div>{parts.map(el =>  <Part key={el.id} part={el} /> )}</div>
-
-const Part = ({part}) => <div> {part.name} {part.exercises}</div>
- 
-const Total = ({total}) =><strong><p>total of {total} exercises</p></strong>
-
-const Course = ({courses}) => {
-  return  (<div>
-            <h1>Web development curriculum</h1>
-            {courses.map((course) => (
-                <div key={course.id}>
-                  <Header  course = {course.name} />
-                  <Content  parts={course.parts} />
-                  <Total  total={course.parts.reduce((prev, curr) => prev + curr.exercises ,0)} />
-                </div>
-            ))}
-
-        </div>)
-}
-
-
+import Course from './Course'
 const App = () => {
   const courses = [
     {
