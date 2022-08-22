738e5dd2cf06359215432cc27b4ae83fb5fe9b7a
sm-mostafajamal <sm.mostafajamal@gmail.com>
2022-08-23 02:08:29 +0600

    2.13*: Data for countries, step2

--- part2/data-for-countries/src/App.js
+++ part2/data-for-countries/src/App.js
@@ -4,7 +4,8 @@ import { useState, useEffect } from 'react';
 function App() {
   const [countryInfos, setCountryInfos] = useState([])
   const [search, setSearch] = useState('')
-
+  const [showSelected, setShowSelected] = useState(false)
+  const [selectedCountry, setSelectedCountry] = useState([])
   useEffect(()=>{
     axios
     .get('https://restcountries.com/v3.1/all')
@@ -17,27 +18,43 @@ function App() {
 
   const searchResult = countryInfos.filter(country =>((country.name.common).toLowerCase()).includes(search.toLowerCase()))
   
-  
-  const countriesToShow =  searchResult.length === 1 ?
-   <div>
-      <h1>{searchResult[0].name.common}</h1>
-      <div>capital {searchResult[0].capital}</div>
-      <div>area {searchResult[0].area}</div>
-      <h3>languages:</h3>
-      <ul>
-        {Object.values(searchResult[0].languages).map((lang, id) => <li key={id}>{lang}</li>)}
-      </ul>
-      <img src={searchResult[0].flags.png} alt={`${searchResult[0].name.common} flag`} />
-   </div> 
-   : searchResult.map(country => <li key={country.name.common}>{country.name.common}</li>)
-
+  const showDetails = (country) => {
+          return( <div>
+                    <h1>{country.name.common}</h1>
+                    <div>capital {country.capital}</div>
+                    <div>area {country.area}</div>
+                    <h3>languages:</h3>
+                    <ul>
+                      {Object.values(country.languages).map((lang, id) => <li key={id}>{lang}</li>)}
+                    </ul>
+                    <img src={country.flags.png} alt={`${country.name.common} flag`} />
+                  </div>
+                )
+              }
+ 
+  const handleSearch = (e) => {
+    setSearch(e.target.value)
+    setShowSelected(false)
+    setSelectedCountry([])
+  }
+  const handleButton = (country) => {
+    setShowSelected(!showSelected)
+    setSelectedCountry(selectedCountry.concat(country))
+  }
   return (
     <div>
       <div>
-        find countries <input value={search} onChange={(e) => setSearch(e.target.value)} />
+        find countries <input value={search} onChange={handleSearch} />
       </div>
       <div>
-        {searchResult.length > 10 ? 'Too many matches, specify another filter' : countriesToShow}
+        {searchResult.length > 10 ? 'Too many matches, specify another filter' 
+        : searchResult.length === 1 ? (showDetails(searchResult[0]))
+        : showSelected ?  showDetails(selectedCountry[0])
+        : searchResult.map(country => <li key={country.name.common}>
+                                        {country.name.common}
+                                        <input type='button' value='show' onClick={()=>handleButton(country)} />
+                                      </li>)}
+        
       </div>
     </div>
   );
