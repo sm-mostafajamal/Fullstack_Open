6a4e5f1d6773576d42bd536e622ccbd53dd819c6
sm-mostafajamal <sm.mostafajamal@gmail.com>
2022-08-24 03:18:55 +0600

    2.14*: Data for countries, step3

--- part2/data-for-countries/src/App.js
+++ part2/data-for-countries/src/App.js
@@ -3,39 +3,73 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [countryInfos, setCountryInfos] = useState([])
+  const [selectedCountry, setSelectedCountry] = useState([])
+  const [weather, setWeather] = useState(null)
   const [search, setSearch] = useState('')
   const [showSelected, setShowSelected] = useState(false)
-  const [selectedCountry, setSelectedCountry] = useState([])
+  const api_key = process.env.REACT_APP_API_KEY
+
+  
+  const searchResult = countryInfos.filter(country =>((country.name.common).toLowerCase()).includes(search.toLowerCase()))
+  
   useEffect(()=>{
     axios
     .get('https://restcountries.com/v3.1/all')
     .then(res=> {
-      console.log('promised fulfilled')
       setCountryInfos(res.data)
     })
-    
-  },[])
-
-  const searchResult = countryInfos.filter(country =>((country.name.common).toLowerCase()).includes(search.toLowerCase()))
+  },[api_key])
   
+  useEffect(()=>{
+    const api = (cityName) => {
+      return axios
+              .get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName.name.common}&appid=${api_key}`)
+              .then(res => {
+                setWeather(res.data)
+              }) 
+      }
+
+    if(selectedCountry.length){
+      api(selectedCountry[0])
+    }else if(searchResult.length === 1){
+      api(searchResult[0])
+    }
+
+  },[api_key, selectedCountry, searchResult])
+
   const showDetails = (country) => {
-          return( <div>
-                    <h1>{country.name.common}</h1>
-                    <div>capital {country.capital}</div>
-                    <div>area {country.area}</div>
-                    <h3>languages:</h3>
-                    <ul>
-                      {Object.values(country.languages).map((lang, id) => <li key={id}>{lang}</li>)}
-                    </ul>
-                    <img src={country.flags.png} alt={`${country.name.common} flag`} />
+
+          return <div>
+                      <div>
+                        <h1>{country.name.common}</h1>
+                        <div>capital {country.capital}</div>
+                        <div>area {country.area}</div>
+                        <h3>languages:</h3>
+                        <ul>
+                          {Object.values(country.languages).map((lang, id) => <li key={id}>{lang}</li>)}
+                        </ul>
+                        <img src={country.flags.png} alt={`${country.name.common} flag`} />
+                      </div>
+                      {
+                        weather !== null &&
+                      <div>
+                        <h2>Weather in {country.capital}</h2>
+                        <div>temperature {(weather.main.temp - 273.15).toFixed(2)} Celcius</div>
+                        <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt='weather icon' />
+                        <div>wind {weather.wind.speed} m/s</div>
+                      </div>
+                      }
+                      
                   </div>
-                )
-              }
- 
+       
+  }
+
+
+
   const handleSearch = (e) => {
     setSearch(e.target.value)
     setShowSelected(false)
-    setSelectedCountry([])
+    setSelectedCountry([])    
   }
   const handleButton = (country) => {
     setShowSelected(!showSelected)
@@ -43,19 +77,22 @@ function App() {
   }
   return (
     <div>
+
       <div>
         find countries <input value={search} onChange={handleSearch} />
       </div>
+      
       <div>
         {searchResult.length > 10 ? 'Too many matches, specify another filter' 
-        : searchResult.length === 1 ? (showDetails(searchResult[0]))
+        : searchResult.length === 1 ? showDetails(searchResult[0])
         : showSelected ?  showDetails(selectedCountry[0])
-        : searchResult.map(country => <li key={country.name.common}>
-                                        {country.name.common}
-                                        <input type='button' value='show' onClick={()=>handleButton(country)} />
-                                      </li>)}
-        
+        : searchResult.map(country => 
+            <li key={country.name.common}>
+                {country.name.common}
+                <input type='button' value='show' onClick={()=>handleButton(country)} />
+            </li>)}
       </div>
+
     </div>
   );
 }
