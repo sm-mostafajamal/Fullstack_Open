526195d6d65b9e50cedba51ac3816b993e2b3746
sm-mostafajamal <sm.mostafajamal@gmail.com>
2022-08-29 21:54:10 +0600

    3.6: Phonebook backend step6

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -63,7 +63,7 @@ app.post('/api/persons', (req, res) => {
     })
   }
   else if(persons.find(p=> (p.name).toLowerCase() === ((contactInfo.name).trim()).toLowerCase())){
-    return res.status(400).json({
+    return res.status(409).json({
       error: 'name must be unique' 
     })
   }
@@ -75,8 +75,8 @@ app.post('/api/persons', (req, res) => {
 
 app.delete('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
+    
     persons = persons.filter(p => p.id !== id)
-
     res.status(204).end()
     
 })
