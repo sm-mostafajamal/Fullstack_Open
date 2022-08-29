ea1699d4d30f26cc9f405946132e26887aa86132
sm-mostafajamal <sm.mostafajamal@gmail.com>
2022-08-29 13:22:33 +0600

    3.2: Phonebook backend step2

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -29,6 +29,11 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+    res.send(`<p>Phonebook has info for ${persons.length} people<p> ${new Date()}`)
+})
+
+
 
 app.listen(PORT, ()=> {
     console.log(`Server running on port ${PORT}`)
