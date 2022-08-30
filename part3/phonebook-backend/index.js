35fee4b453a46fd811471fb262560c240bc6291d
sm-mostafajamal <sm.mostafajamal@gmail.com>
2022-08-30 21:38:57 +0600

    changes

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan')
 const cors = require('cors')
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 
 let  persons = [
