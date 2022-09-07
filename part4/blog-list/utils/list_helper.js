01bf6aee225ad52880088c8888311e56debc930c
sm-mostafajamal <sm.mostafajamal@gmail.com>
2022-09-08 00:56:33 +0600

    4.5*: helper functions and unit tests, step3

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,3 +1,4 @@
+
 const dummy = (blogs) => blogs.length === 0 ? 1 : 0;
 
 const totalLikes = (blogs) => {
@@ -5,6 +6,17 @@ const totalLikes = (blogs) => {
   return likes.reduce((prev, curr) => prev + curr, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  const likes = blogs.map((blog) => blog.likes);
+  const highestLike = Math.max(...likes);
+  const highestLikeInfo = blogs.find((profile) => profile.likes === highestLike);
+  return {
+    title: highestLikeInfo.title,
+    author: highestLikeInfo.author,
+    likes: highestLikeInfo.likes
+  };
+};
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 };
