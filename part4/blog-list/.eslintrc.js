0265aa10acd899151a413cea4f9bab9e0255347e
sm-mostafajamal <sm.mostafajamal@gmail.com>
2022-09-07 22:22:47 +0600

    4.3: helper functions and unit tests, step1

--- part4/blog-list/.eslintrc.js
+++ part4/blog-list/.eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     commonjs: true,
     es2021: true,
     node: true,
+    jest: true,
   },
   extends: [
     'plugin:react/recommended',
@@ -18,5 +19,6 @@ module.exports = {
     'react',
   ],
   rules: {
+    'prefer-arrow-callback': 'error',
   },
 };
