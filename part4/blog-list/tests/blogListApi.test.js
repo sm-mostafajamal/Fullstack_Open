08b07c38218fc72ece5a38766c1165c922b4552c
sm-mostafajamal <sm.mostafajamal@gmail.com>
2022-09-09 19:30:37 +0600

    4.9*: Blog list tests, step2

--- part4/blog-list/tests/blogListApi.test.js
+++ part4/blog-list/tests/blogListApi.test.js
@@ -4,17 +4,20 @@ const app = require('../app');
 
 const api = supertest(app);
 
-test('blogs are returned as json', async () => {
+test('blogs are returned as json with correct amount', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/);
-}, 100000);
 
-test('there are two blogs', async () => {
   const response = await api.get('/api/blogs');
 
-  expect(response.body).toHaveLength(2);
+  expect(response.body).toHaveLength(7);
+}, 100000);
+
+test('unique identifier property of the blog posts is named id (not _id)', async () => {
+  const response = await api.get('/api/blogs');
+  expect(response.body[0].id).toBeDefined();
 });
 
 afterAll(() => {
