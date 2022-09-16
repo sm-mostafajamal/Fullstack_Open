ca1cb64d9a21bd6504a76ec2a939823c1eca5a46
sm-mostafajamal <sm.mostafajamal@gmail.com>
2022-09-17 00:37:53 +0600

    4.15: bloglist expansion, step3

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -6,6 +6,7 @@ const app = express();
 const { info, error } = require('./utils/logger');
 const { MONGODB_URI } = require('./utils/config');
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
 const { errorHandler } = require('./utils/middleware');
 
 info('connecting to', MONGODB_URI);
@@ -22,6 +23,7 @@ mongoose
 app.use(cors());
 app.use(express.json());
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 app.use(errorHandler);
 
 module.exports = app;
---
+++ part4/blog-list/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const usersRouter = require('../controllers/users');
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  name: String,
+  passwordHash: String,
+  // blogs: [
+  //   {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: 'Blog'
+  //   }
+  // ]
+})
+
+userSchema.set('toJSON', {
+  transform:(document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.passwordHash
+  }
+})
+
+module.exports = mongoose.model('User', userSchema);
