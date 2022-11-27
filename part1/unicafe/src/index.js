475992ef2c7883ac31b52243ac8d24e45ac0b6dc
sm-mostafajamal <sm.mostafajamal@gmail.com>
2022-11-27 20:40:42 +0600

    6.1: unicafe revisited, step1

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,7 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';
+import { legacy_createStore as createStore } from 'redux'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0
+}
 
+const counterReducer = (state = initialState, action) => {
+  switch(action.type) {
+      case 'GOOD': 
+        return { ...state, good: state.good + 1};
+      case 'OK': 
+        return { ...state, ok: state.ok + 1};
+      case 'BAD': 
+        return { ...state, bad: state.bad + 1};
+      default: 
+        return state; 
+  }
+}
+const store = createStore(counterReducer)
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          Good: {store.getState().good}
+        </div>
+        <div>
+          OK: {store.getState().ok}
+        </div>
+        <div>
+          BAD: {store.getState().bad}
+        </div>
+      </div>
+      <div>
+        <button onClick={() => store.dispatch({ type: 'GOOD'})}>GOOD</button>
+        <button onClick={() => store.dispatch({ type: 'OK'})}>OK</button>
+        <button onClick={() => store.dispatch({ type: 'BAD'})}>BAD</button>
+      </div>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+const renderApp = () => root.render(<App />)
+renderApp()
+store.subscribe(renderApp)
 
