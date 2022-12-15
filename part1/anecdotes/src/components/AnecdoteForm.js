0c5ae6605fdf44064392d499cd0a7e386be2c7a3
sm-mostafajamal <sm.mostafajamal@gmail.com>
2022-12-16 05:46:52 +0600

    6.8: anecdotes, step6

--- part1/anecdotes/src/components/AnecdoteForm.js
+++ part1/anecdotes/src/components/AnecdoteForm.js
@@ -1,5 +1,18 @@
-const AnecdotesForm = ({ onSubmit }) => {
-  return <form onSubmit={onSubmit}>
+import { useDispatch } from 'react-redux'
+import { newAnecdote } from '../reducers/anecdoteReducer'
+
+
+const AnecdotesForm = () => {
+  const dispatch = useDispatch()
+
+   const addAnecdote = (e) => {
+    e.preventDefault()
+    const content = e.target.anecdote.value
+    e.target.anecdote.value = ''
+    dispatch(newAnecdote(content))
+  }
+  return <form onSubmit={addAnecdote}>
+          <h2>create new</h2>
           <div>
             <input name='anecdote'/>
             <button type='submit'>create</button>
---
+++ part1/anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,35 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { anecdote } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+  
+  const vote = (id) => {
+    dispatch(anecdote(id))
+  }
+
+  anecdotes.sort((a, b) => b.votes-a.votes)
+
+  return (
+    <div>
+      {
+        anecdotes.map(anecdote => 
+        
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+
+      )}
+    </div>
+  )
+}
+
+export default AnecdoteList;
