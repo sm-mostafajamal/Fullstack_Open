4e6a131dd6dc886003b175fabd2eff6bcf92b375
sm-mostafajamal <sm.mostafajamal@gmail.com>
2023-02-11 10:45:46 +0600

    warning

--- part6/query-anecdotes/src/App.js
+++ part6/query-anecdotes/src/App.js
@@ -1,19 +1,37 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { useQuery } from 'react-query'
+import { getAll } from './Server/services'
+
 
 const App = () => {
+
+  const { isLoading, isError, data, error } = useQuery(
+    'anecdotes', 
+    getAll, 
+    {
+      retry: false
+    }
+  )
+
+  if(isLoading){ 
+    return <span>Loading....</span>
+  }
+  if(isError){ 
+    return <span>anecdote service not available due to problems in server</span>
+  }
+  
 
   const handleVote = (anecdote) => {
     console.log('vote')
   }
-
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  // const anecdotes = [
+  //   {
+  //     "content": "If it hurts, do it more often",
+  //     "id": "47145",
+  //     "votes": 0
+  //   },
+  // ]
 
   return (
     <div>
@@ -22,7 +40,7 @@ const App = () => {
       <Notification />
       <AnecdoteForm />
     
-      {anecdotes.map(anecdote =>
+      { data.map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
